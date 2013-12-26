d3fee56ce3479983ec17f3de87ef197f912e4315
Marcel <admin@lonie.de>
2013-12-26 16:17:34 +0100

    Open Graph type angepasst, og:url übergeben

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,11 @@ class IndexController extends Zend_Controller_Action {
         /* Initialize action controller here */
         $this->_helper->layout()->getView()->headTitle("Steffen Erdmann", 
                     Zend_View_Helper_Placeholder_Container_Abstract::SET);
+        $layout = $this->_helper->layout()->ogurl = 
+                $this->getRequest()->getScheme() . 
+                '://' . 
+                $this->getRequest()->getHttpHost() .
+                $this->getRequest()->getRequestUri();
     }
 
     public function indexAction() {
